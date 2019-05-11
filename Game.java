098ddf11b380bad6a2cc1a3d395a2feaed0558e7
Sepehr Madani <ssepehrmadani@gmail.com>
2019-05-11 15:34:08 -0400

    Now supports a scoring system, and the GUI has a title

--- Game.java
+++ Game.java
@@ -1,4 +1,4 @@
-import java.awt.Color;
+import java.awt.*;
 import java.awt.event.WindowEvent;
 import java.util.ArrayList;
 import java.util.List;
@@ -7,10 +7,12 @@ import javax.swing.*;
 
 public class Game {
 
-    // Frame Window Size Properties
-    private static final int WINDOW_WIDTH = 600;                   //Total Window's Width
-    private static final int WINDOW_HEIGHT = WINDOW_WIDTH + 100;   //Total Window's Height, a little more for the title
+    // Panel size constants
+    private static final int WINDOW_WIDTH = 600;                   // Total Window's Width
+    private static final int WINDOW_HEIGHT = WINDOW_WIDTH + 120;   // Total Window's Height
     private static final int MARGIN = 64;
+    private static final int TOP_MARGIN = 88;
+    private static final int TITLE_MARGIN = 18;
 
     // Cell Size
     private static final int CELL_SIZE = (WINDOW_WIDTH - 3*MARGIN) / 2;
@@ -20,13 +22,15 @@ public class Game {
     private static int PlayerAttempt;                               // The Cell Number that Player attempts to guess
     private static int AttemptIndex = 0;                            // Index of the User's guess to check with Sequence
     private static Random NumberGenerator = new Random();           // Random generator for flashing
+    private static int ScoreValue = 0;                              // Player's Score
 
-    //Graphical Elements Definition
-    private static JFrame frame = new JFrame();
+    // Graphical Elements Definition
+    private static JFrame gameFrame = new JFrame();
     private static JButton RedCell = new JButton();
     private static JButton GreenCell = new JButton();
     private static JButton BlueCell = new JButton();
     private static JButton YellowCell = new JButton();
+    private static JLabel scoreLabel;
 
     // Logical booleans of the Game
     private static boolean canReceiveClicks = false;                // To prevent user clicks when showing Sequence
@@ -43,6 +47,9 @@ public class Game {
         ///  START  ///
         ///////////////
         do {
+            scoreLabel.setText("Score: " + ScoreValue);            // Increase the score
+            ScoreValue++;
+
             Sequence.add(NumberGenerator.nextInt(4));       // Adds the next random to display
             playCells();                                           // Displays the Sequence by flashing the Cells
 
@@ -55,37 +62,54 @@ public class Game {
             AttemptIndex = 0;
         } while (noPlayerMistake);
     }
-    
+
     private static void graphicElementsInitialize() { // Initializing, placing, and displaying the GUI
 
+        gameFrame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);       // Terminate upon exit
+
+        //JPanel inside the gameFrame
+        JPanel gameContentPanel = new JPanel();
+        gameFrame.setContentPane(gameContentPanel);  // Adds the gameContentPanel to gameFrame
+
         // JFrame Construction, Size, Location, Exit state
-        frame.getContentPane().add(new JLabel("Welcome to the Secret Shop!"));
-        frame.setTitle("Simon Game, by Sepehr Madani");             // Window Title
-        frame.setSize(WINDOW_WIDTH, WINDOW_HEIGHT);                 // Size
-        frame.setLocation(200, 200);                          // Location
-        frame.setBackground(Color.WHITE);                           // Color
-        frame.setResizable(false);                                  // Non-resizable
-        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);       // Terminate upon exit
+        gameFrame.setTitle("Simon Game, by Sepehr Madani");             // Window Title
+        gameFrame.setSize(WINDOW_WIDTH, WINDOW_HEIGHT);                 // Size
+        gameFrame.setLocation(200, 200);                          // Location
+        gameFrame.setResizable(false);                                  // Non-resizable
+
+        // Title of the Game in the gameFrame
+        JLabel GameTitle = new JLabel("Simon Game", JLabel.CENTER);
+        GameTitle.setSize(WINDOW_WIDTH, MARGIN + TITLE_MARGIN);
+        GameTitle.setFont(new Font(GameTitle.getFont().getName(), Font.PLAIN, 64));
+        GameTitle.setLocation(0, TITLE_MARGIN);
+        gameContentPanel.add(GameTitle);
+
+        //Score in the gameFrame
+        scoreLabel = new JLabel("Score: 0", JLabel.CENTER);
+        scoreLabel.setSize(WINDOW_WIDTH, MARGIN + TITLE_MARGIN);
+        scoreLabel.setFont(new Font(GameTitle.getFont().getName(), Font.PLAIN, 24));
+        scoreLabel.setLocation(0, 4 * TITLE_MARGIN);
+        gameContentPanel.add(scoreLabel);
 
         // Red Button Construction
         RedCell.setBackground(Color.RED);
-        RedCell.setBounds(MARGIN, MARGIN, CELL_SIZE, CELL_SIZE);
-        frame.add(RedCell);
+        RedCell.setBounds(MARGIN, MARGIN + TOP_MARGIN, CELL_SIZE, CELL_SIZE);
+        gameFrame.add(RedCell);
 
         // Green Button Construction
         GreenCell.setBackground(Color.GREEN);
-        GreenCell.setBounds(2*MARGIN + CELL_SIZE, MARGIN, CELL_SIZE, CELL_SIZE);
-        frame.add(GreenCell);
+        GreenCell.setBounds(2 * MARGIN + CELL_SIZE, MARGIN + TOP_MARGIN, CELL_SIZE, CELL_SIZE);
+        gameFrame.add(GreenCell);
 
         // Blue Button Construction
         BlueCell.setBackground(Color.BLUE);
-        BlueCell.setBounds(MARGIN, 2*MARGIN + CELL_SIZE, CELL_SIZE, CELL_SIZE);
-        frame.add(BlueCell);
+        BlueCell.setBounds(MARGIN, 2*MARGIN + CELL_SIZE + TOP_MARGIN, CELL_SIZE, CELL_SIZE);
+        gameFrame.add(BlueCell);
 
         // Yellow Button Construction
         YellowCell.setBackground(Color.YELLOW);
-        YellowCell.setBounds(2*MARGIN + CELL_SIZE, 2*MARGIN + CELL_SIZE, CELL_SIZE, CELL_SIZE);
-        frame.add(YellowCell);
+        YellowCell.setBounds(2*MARGIN + CELL_SIZE, 2*MARGIN + CELL_SIZE + TOP_MARGIN, CELL_SIZE, CELL_SIZE);
+        gameFrame.add(YellowCell);
 
         // Action Listeners for user click
         RedCell.addActionListener(e -> cellClick(RedCell));
@@ -93,17 +117,17 @@ public class Game {
         BlueCell.addActionListener(e -> cellClick(BlueCell));
         YellowCell.addActionListener(e -> cellClick(YellowCell));
 
-        // Finalizing frame visibility
-        frame.setLayout(null);
-        frame.setVisible(true);
+        // Finalizing gameFrame visibility
+        gameFrame.setLayout(null);
+        gameFrame.setVisible(true);
     }
-    
+
     private static void playCells() { // Displays the Sequence by flashing the Cells
-        canReceiveClicks = false;                          // Disables player's ability to click and lose
-        sleep(500);                                     // To wait after Player's last click
-        frame.getContentPane().setBackground(Color.GRAY);  // Makes the frame gray --> Player now looks
-        sleep(500);                                     // To grab player's attention after it turned gray
-        for (Integer aSequence : Sequence) {               // In a foreach loop, displays the Sequence on Cells
+        canReceiveClicks = false;                               // Disables player's ability to click and lose
+        sleep(500);                                          // To wait after Player's last click
+        gameFrame.getContentPane().setBackground(Color.GRAY);   // Makes the gameFrame gray --> Player now looks
+        sleep(500);                                          // To grab player's attention after it turned gray
+        for (Integer aSequence : Sequence) {                    // In a foreach loop, displays the Sequence on Cells
             switch (aSequence) {
                 case 0: // for Red
                     cellShow(RedCell);
@@ -120,7 +144,7 @@ public class Game {
             }
         }
         sleep(500);                                      // Last delay to make a smooth transition to white
-        frame.getContentPane().setBackground(Color.WHITE);  // Returns color back to normal (Show's over!)
+        gameFrame.getContentPane().setBackground(Color.WHITE);  // Returns color back to normal (Show's over!)
         canReceiveClicks = true;                            // Re-enables player's ability to click
     }
     private static void cellShow(JButton A) {       // When GAME plays the sequence for player
@@ -133,8 +157,12 @@ public class Game {
 
     private static void cellClick(JButton A) {    // When USER clicks to guess
         if (canReceiveClicks) {
-            Color PrevColor = A.getBackground();
-            cellClickAnimation(A, PrevColor);
+
+            Color PrevColor = A.getBackground();    // The Animation
+            A.setBackground(Color.WHITE);           // for the Cell
+            sleep(100);                          // to shot that it
+            A.setBackground(PrevColor);             // has been clicked
+
             if (PrevColor == Color.RED) { PlayerAttempt = 0; }
             if (PrevColor == Color.GREEN) { PlayerAttempt = 1; }
             if (PrevColor == Color.BLUE) { PlayerAttempt = 2; }
@@ -142,17 +170,12 @@ public class Game {
             checkForPlayerMistake();
         }
     }
-    private static void cellClickAnimation (JButton A, Color PrevColor) {
-        A.setBackground(Color.WHITE);
-        sleep(100);
-        A.setBackground(PrevColor);
-    }
 
     private static void checkForPlayerMistake() { // Checks if Player
-        if (Sequence.get(AttemptIndex) == PlayerAttempt) {
+        if (Sequence.get(AttemptIndex) == PlayerAttempt) {  // if player got it right, check for their next click
             AttemptIndex++;
         } else {
-            noPlayerMistake = false;
+            noPlayerMistake = false;                        // else, then player made a mistake. Oops!
         }
     }
     private static void gameOver() {
@@ -161,9 +184,11 @@ public class Game {
         GreenCell.setBackground(Color.BLACK);
         BlueCell.setBackground(Color.BLACK);
         YellowCell.setBackground(Color.BLACK);
-        JOptionPane.showMessageDialog(null, "Oops! You lost. Great Job!","Game over", JOptionPane.ERROR_MESSAGE);
+        ScoreValue--;                                       // Fix the off-by-one during mistake
+        String gameOverMessage = "Oops! You lost. Final score: " + ScoreValue;
+        JOptionPane.showMessageDialog(null, gameOverMessage,"Game over", JOptionPane.ERROR_MESSAGE);
         sleep(500);
-        frame.dispatchEvent(new WindowEvent(frame, WindowEvent.WINDOW_CLOSING));
+        gameFrame.dispatchEvent(new WindowEvent(gameFrame, WindowEvent.WINDOW_CLOSING));
     }
 
     private static void sleep(int n) {
